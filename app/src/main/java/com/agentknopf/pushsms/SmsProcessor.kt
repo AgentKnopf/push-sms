fb0141eac1408a8f90c3c66a959a563feebbaaf3
AgentKnopf <agent.knopf@gmail.com>
2018-01-04 21:33:49 +0100

    Implemented sms matching, added logging. Previous issue remains: Instantiating the rules upon re-starting the app fails.

--- app/src/main/java/com/agentknopf/pushsms/SmsProcessor.kt
+++ app/src/main/java/com/agentknopf/pushsms/SmsProcessor.kt
@@ -1,8 +1,10 @@
 package com.agentknopf.pushsms
 
+import android.support.annotation.WorkerThread
 import android.util.Log
 import com.agentknopf.pushsms.model.Sms
 import com.agentknopf.pushsms.model.SmsRule
+import com.agentknopf.pushsms.store.Persister
 
 private const val TAG = "SmsHandler"
 
@@ -20,9 +22,18 @@ internal data class SmsProcessor(val sms: Sms) {
         applyRules(findMatchingRules(), sms)
     }
 
-    private fun findMatchingRules(): List<SmsRule> = TODO("not implemented - will need database access to rules")
+    @WorkerThread
+    private fun findMatchingRules(): List<SmsRule> {
+        val store = Persister.loadStore()
+        var result = mutableListOf<SmsRule>()
+        result.addAll(store.getRules())
+        result.filter { !it.doesRuleApply(sms) }
+        Log.i(TAG, "Found ${result.size} matching rules")
+        return result
+    }
 
     private fun applyRules(rules: List<SmsRule>, sms: Sms) {
+        Log.i(TAG, "Executing ${rules.size} on $sms")
         for (rule in rules) rule.executeRule(sms)
     }
 }
